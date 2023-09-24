365355ceca484090107d0834cf85474ac9a6a639
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-09-24 14:51:19 +0100

    Update on login-blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
                         </form>
                         <p class="w-100 text-center">&mdash; Not a user ? &mdash;</p>
                         <div class="social d-flex text-center">
-                            <a href="{{ route('register') }}" style="color: #fff">{{ __('Register') }}</a>
+                            <a href="{{ route('register') }}" class="px-2 py-2 mr-md-1"><span class="ion-logo-facebook mr-2"></span> {{ __('Register') }}</a>
                         </div>
                     </div>
                 </div>
