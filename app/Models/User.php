653b8b7f892e5805c3c5ca544547a3ea0a204695
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-09-25 19:03:33 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -43,4 +45,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
+    public function token(): Attribute
+    {
+        return Attribute::get(
+            fn() => $this->createToken("Personal access token")->plainTextToken
+        );
+    }
 }
