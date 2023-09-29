8974d52a528739da8484c7b74e53176a5cfa0b17
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-09-29 11:18:16 +0100

    Created API/TaskController

---
+++ app/Http/Controllers/API/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    public function all_task()
+    {
+        $tasks = Task::all();
+
+        return response()->json(['data' => $tasks], 200);
+    }
+}
