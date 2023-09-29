89814bb42a052fe96b9ece5474a5763a87718e99
Ken Ossai <49806812+keniossai@users.noreply.github.com>
2023-09-29 17:10:48 +0100

    Update TaskController.php

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
@@ -12,5 +13,12 @@ class TaskController extends Controller
     {
         $tasks = Task::all();
         return response()->json(['data' => $tasks], 200);
+    }
+
+    public function tasksForUser()
+    {
+        $user = Auth::user();
+        $tasks = Task::where('user_id', $user->id)->get();
+        return response()->json(['tasks' => $tasks]);
     }
 }
